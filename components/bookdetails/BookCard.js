cf2defa0a8587376e60288058655d6dccaea0b8f
root <mehedishafi@codemarshal.com>
2019-11-25 21:34:03 +0600

    feat: add download library, download url generation method

--- components/bookdetails/BookCard.js
+++ components/bookdetails/BookCard.js
@@ -6,6 +6,7 @@ import {
     Text,
     Paragraph,
 } from 'react-native-paper';
+import RNBackgroundDownloader from 'react-native-background-downloader';
 
 import Styles from './styles';
 
@@ -17,6 +18,22 @@ export default class BookCardComponent extends React.Component{
         };
     }
 
+    startDownload = () => {
+        let task = RNBackgroundDownloader.download({
+            id: this.state.book.id,
+            url: this.state.book.direct_url,
+            destination: `${RNBackgroundDownloader.directories.documents}/${this.state.book.title}.${this.state.book.file_type}`
+        }).begin((expectedBytes) => {
+            console.log(`Going to download ${expectedBytes} bytes!`);
+        }).progress((percent) => {
+            console.log(`Downloaded: ${percent * 100}%`);
+        }).done(() => {
+            console.log('Download is done!');
+        }).error((error) => {
+            console.log('Download canceled due to error: ', error);
+        });
+    }
+
     render(){
         return(
             <Card >
@@ -26,9 +43,9 @@ export default class BookCardComponent extends React.Component{
                     <Paragraph>
                         {this.state.book.description}
                     </Paragraph>
-                    <Card.Cover source={{ uri: 'https://picsum.photos/700'}} />
+                    <Card.Cover source={{ uri: this.state.book.thumb_url}} />
                     <Card.Actions>
-                        <Button>Download</Button> 
+                        <Button onPress={this.startDownload}>Download</Button> 
                         <Button>Torrent</Button>
                     </Card.Actions>
                 </Card.Content>
