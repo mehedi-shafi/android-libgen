b4f45f4b0dd58bbab35439d702812eb9f0abe725
root <mehedishafi@hotmail.com>
2019-11-30 21:04:47 +0600

    feat: new book details screen

--- components/bookdetails/BookCard.js
+++ components/bookdetails/BookCard.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import {
-    Paper,
-    Card,
-    Button,
+    Surface,
     Text,
     Paragraph,
+    Button,
+    Provider,
+    Portal,
+    Modal,
 } from 'react-native-paper';
 
+import {
+    View,
+    Image,
+    ScrollView,
+} from 'react-native';
+
+import ViewMoreText from 'react-native-view-more-text';
+
 import Styles from './styles';
+
 import {
     Linking,
     } from 'expo';
-
 
 export default class BookCardComponent extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            book: props.book
+            book: props.book,
+            downloadModalVisible: false,
         };
     }
 
@@ -29,22 +40,88 @@ export default class BookCardComponent extends React.Component{
         Linking.openURL(this.state.book.torrent_url);
     }
 
+    renderViewMoreOnPress = (onPress) => {
+        return (
+            <View style={Styles.expandableTextView}>
+                <Text                
+                    style={Styles.expandableText}
+                    onPress={onPress}>View more ...</Text>
+            </View>
+        );
+    }
+
+    renderViewLessOnPress = (onPress) => {
+        return (
+            <View style={Styles.expandableTextView}>
+                <Text                
+                    style={Styles.expandableText}
+                    onPress={onPress}>View less ...</Text>
+            </View>
+        )
+    }
+
+    dismissModal = () => {
+        this.setState({downloadModalVisible: false});
+    }
+
+    openModal = () => {
+        this.setState({downloadModalVisible: true});
+    }
+
     render(){
         return(
-            <Card >
-                <Text style={Styles.titleText}>{this.state.book.title}</Text>
-                <Text style={Styles.subTitleText}>{this.state.book.author}</Text>
-                <Card.Content>
-                    <Paragraph>
-                        {this.state.book.description}
-                    </Paragraph>
-                    <Card.Cover source={{ uri: this.state.book.thumb_url}} />
-                    <Card.Actions>
-                        <Button onPress={this.startDownload}>Download</Button> 
-                        <Button onPress={this.startTorrentDownload}>Torrent</Button>
-                    </Card.Actions>
-                </Card.Content>
-            </Card>
+            <Provider
+                style={Styles.activityStyle}>
+                <Portal>
+                    <Modal
+                        visible={this.state.downloadModalVisible}
+                        onDismiss={this.dismissModal}>
+                           
+                        </Modal>
+                    <View
+                        style={Styles.viewHolderCenter}>
+                        <Surface
+                            style={Styles.bookThumbNailHolder}>
+                            <Image
+                                style={Styles.bookThumbNail}
+                                source={{uri: this.state.book.thumb_url}} />
+                        </Surface>
+                    </View>
+                    <View
+                        style={Styles.downloadSection}>
+                        <Button
+                            mode='outlined'
+                            icon='download'
+                            style={Styles.downloadButton}
+                            onPress={this.openModal} >
+                            Download
+                        </Button>
+                    </View>
+                    <View>
+                        <Text
+                            style={Styles.titleText}>
+                                {this.state.book.title}
+                        </Text>
+                        <Text style={{textAlign: 'center'}}>By:{'  '}
+                            <Text
+                                style={Styles.subTitleText}>
+                                {this.state.book.author}
+                            </Text>
+                        </Text>
+                    </View>
+                    <ScrollView
+                        nestedScrollEnabled={true}
+                        style={Styles.bookDescription}>
+                        <ViewMoreText
+                            numberOfLines={4}
+                            renderViewLess={this.renderViewLessOnPress}
+                            renderViewMore={this.renderViewMoreOnPress}
+                            textStyle={{textAlign: 'justify', marginLeft: 10, marginRight: 10}}>                                                
+                            <Paragraph>{this.state.book.description}</Paragraph>
+                        </ViewMoreText>
+                    </ScrollView>
+                </Portal>
+            </Provider>
         )
     }
 }
--- components/bookdetails/styles.js
+++ components/bookdetails/styles.js
@@ -2,13 +2,14 @@ import {StyleSheet} from 'react-native';
 
 export default Styles = StyleSheet.create({
     titleText:{
-        textAlign: 'justify',
+        textAlign: 'center',
         fontSize: 18,
         fontWeight: 'bold',
         margin: 10,             
     },
     subTitleText:{
         fontWeight: 'bold',
+        textAlign: 'center',
         marginBottom: 3,
         marginLeft: 10,
         marginTop: 5
@@ -16,5 +17,51 @@ export default Styles = StyleSheet.create({
     infoText:{
         fontStyle: 'italic',
         padding: 5
+    },
+    viewHolderCenter: {
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'center',
+
+    },
+    bookThumbNail:{
+        width: 150,
+        height: 250,
+        borderRadius: 10,
+    },
+    bookThumbNailHolder:{
+        elevation: 4,
+        marginTop: 30,
+        width: 150,
+        height: 250,
+        borderRadius: 10,
+    },
+    activityStyle:{
+        flex: 1,
+        flexDirection: 'column'
+    },
+    bookDescription: {
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    expandableTextView:{
+        flex: 1,
+        flexDirection: 'row',
+        textAlign: 'right',
+        justifyContent: 'flex-end',
+        marginRight: 10,
+    },
+    expandableText:{
+        color: '#303f9f',
+    },
+    downloadButton:{
+        width: 150, 
+        height: 40,
+    },
+    downloadSection:{
+        marginTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'center'
     }
 });
