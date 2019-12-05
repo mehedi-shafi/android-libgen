f257e33f4427bccca78db9d4153e606be271c524
root <mehedishafi@hotmail.com>
2019-12-05 20:30:23 +0600

    feat: infinite scroll on search result view

--- components/booklist/BookList.js
+++ components/booklist/BookList.js
@@ -5,10 +5,13 @@ import BookRowComponent from '../bookrow/BookRow';
 import styles from './styles';
 import BookCardComponent from '../bookdetails/BookCard';
 
+import Search from '../../api/search';
+
 import {
     Modal,
     Provider,
     Portal,
+    ActivityIndicator,
 } from 'react-native-paper';
 import { TouchableWithoutFeedback, TouchableOpacity } from 'react-native-gesture-handler';
 
@@ -17,12 +20,41 @@ export default class BookList extends React.Component{
         super(props);
         this.state = {
             bookList: props.bookList,
+            nextPage: 2,
+            currentListLoc: 0,
+            searchParam: props.searchParam,
+            errorSnack: false,
+            loading: false
         }
+    }
+
+    fetchMoreBooks = () => {
+        this.setState({loading: true});        
+        let opts = this.state.searchParam;
+        opts['pageNo'] = this.state.nextPage;
+        Search(opts, (response, error) => {
+            if (error){
+                this.setState({errorSnack: true});
+            }
+            else{
+                let tempList = this.state.bookList;
+                let tempNextPage = this.state.nextPage + 1;
+                tempList.push(...response);
+                this.setState({bookList: tempList, loading: false, nextPage: tempNextPage});                
+            }
+        })
     }
 
     openBookDetails = (item) => {
         this.props.navigation.navigate('BookDetails', {
             'book': item});
+    }
+    
+    renderFooter = () =>{
+        if (!this.state.loading) return null;
+        return (
+            <ActivityIndicator animating={true} size='small' />
+        )
     }
 
     render(){
@@ -36,7 +68,10 @@ export default class BookList extends React.Component{
                         </TouchableWithoutFeedback>                            
                 }
                 keyExtractor={
-                    (item, index) => index.toString()}/>                    
+                    (item, index) => index.toString()}
+                ListFooterComponent={this.renderFooter}
+                onEndReached={this.fetchMoreBooks}
+                onEndReachedThreshold={3}/>                    
         )
     }
 }
