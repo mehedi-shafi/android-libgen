3096120d97424e0e3983a28a100a19b96a4479a6
root <mehedishafi@hotmail.com>
2019-12-07 14:13:29 +0600

    refactor: complete moving to react-native

--- android/app/src/main/java/com/android_libgen/MainActivity.java
+++ android/app/src/main/java/com/android_libgen/MainActivity.java
@@ -1,4 +1,4 @@
-package com.android_ligen;
+package com.android_libgen;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "android_ligen";
+    return "android_libgen";
   }
 }
--- android/app/src/main/java/com/android_libgen/MainApplication.java
+++ android/app/src/main/java/com/android_libgen/MainApplication.java
@@ -1,4 +1,4 @@
-package com.android_ligen;
+package com.android_libgen;
 
 import android.app.Application;
 import android.content.Context;
